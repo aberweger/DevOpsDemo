8f036bee5b3d2f905ad733af2dd2b175ef280126
aberweger <145767058+aberweger@users.noreply.github.com>
2024-04-03 17:11:03 +0200

    Update Backend TennisGame + MathLib

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/MathLib.java
@@ -0,0 +1,9 @@
+package ch.zhaw.iwi.devops.demo;
+
+public class MathLib {
+
+    public static boolean isEven(int value){
+        return value % 2 == 0; //wenn Zahl Rest hat dann ungerade. wenn sie kein Rest hat = gerade
+    }
+    
+}
---
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/TennisGame.java
@@ -0,0 +1,6 @@
+package ch.zhaw.iwi.devops.demo;
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
